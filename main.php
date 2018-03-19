fc4961b254c9311856319d4f9831931e3f2b8934
oliver <obenn009@uottawa.ca>
2018-03-19 16:56:25 -0400

    First test of db connection

--- main.php
+++ main.php
@@ -1,3 +1,26 @@
 <?php
 
-echo "This deploymennt workded";
+$dbconn = pg_connect("host=35.227.50.116 dbname=postgres user=postgres password=csi2132")
+or die('Could not connect: ' . pg_last_error());
+
+$query = 'SELECT * FROM test';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+
+echo "This deployment worked";
