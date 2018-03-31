d0fad4443a9476c3e71d2020a9b87bff0f33ef95
oliver <obenn009@uottawa.ca>
2018-03-31 12:18:36 -0400

    More stuff

---
+++ notes.md
@@ -0,0 +1,20 @@
+### Setup a server on mac
+--
+
+Install postgres
+
+brew install postgres
+
+Start Server
+
+pg_ctl -D /usr/local/var/postgres start
+
+Stop Server
+
+pg_ctl -D /usr/local/var/postgres stop
+
+
+
+
+
+ 
---
+++ restaurant.php
@@ -0,0 +1,26 @@
+<?php
+
+$dbconn = pg_connect("host=localhost dbname=oliver user=oliver password=")
+or die('Could not connect: ' . pg_last_error());
+
+$query = 'SELECT * FROM test';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+
+echo "This deployment worked";
